64d1b90d5da2d143be58544ce43c73be9ec6005c
Yosef <mercytv932@gmail.com>
2026-09-18 00:41:04 -0400

    working on onSearch and useState import

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,10 @@
-function SearchBar() {
+import { useState } from "react";
+interface SearchBarProps {
+  onSearch(value: string): void;
+}
+
+function SearchBar({ onSearch }: SearchBarProps) {
+  const [value, setValue] = useState("");
   return (
     <div>
       <input type="text" placeholder="Search for any IP address or domain" />
