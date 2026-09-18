ae63a4ef7dc00524acf4c427d1b6c3cf915dcf8f
Yosef <mercytv932@gmail.com>
2026-09-17 23:08:21 -0400

    done with useIAddress

---
+++ src/hooks/useIpAddress.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { fetchIpData } from "../services/ipApi";
+import type { IpData } from "../types/ip";
+
+export function useIPAdress() {
+  const [data, setData] = useState<IpData | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  async function searchIp(ip: string) {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const result = await fetchIpData(ip);
+      setData(result);
+    } catch {
+      setError("Unable to find IP address");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return {
+    data,
+    loading,
+    error,
+    searchIp,
+  };
+}
